6db7dba0fcf75c65c1e55b84e057bff42b196414
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-01-06 14:23:10 +0100

    API inside of react app

---
+++ public/api/contactHandle.php
@@ -0,0 +1,37 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+if($_POST) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $temat = $_POST["temat"];
+    $wiadomosc = $_POST["wiadomosc"];
+    
+    if(strlen($wiadomosc) < 2 || strlen($name) < 2) {
+        die();
+    }
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die();
+    }
+
+    $to = "kamil.koziol@asprodukt.com";
+    
+    $headers = 
+        'Content-Type: text/plain; charset=utf-8' . "\r\n" .
+        'MIME-Version: 1.0' . "\r\n" .
+        "From: $name <$email>" . "\r\n" .
+        "Reply-To: $name <$email>" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
+
+    $emailBody = 
+        "Email: $email\n".
+        "Nadawca: $name\n".
+        "Wiadomość: $wiadomosc";
+    
+    
+    mail($to, $temat, $emailBody, $headers);
+}
